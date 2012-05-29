84fd562f7bee110a76a5db003d88749f0d59485d
almar.klein <devnull@localhost>
2012-05-29 12:55:00 +0200

    Implemented downloading the freeimage library at runtime.

--- freeimage_install.py
+++ freeimage_install.py
@@ -6,14 +6,16 @@ try:
 except ImportError:
     from urllib.request import urlopen # Py3k
 
-# update base_address to tagged, scikits-image-hosted fork as needed
-# todo: chose an address that is http (not https), some might not have SSL
-BASE_ADDRESS = 'https://raw.github.com/zachrahan/freeimage-sharedlib/master/'
+LOCALDIR = os.path.abspath(os.path.dirname(__file__))
 
-FILES = {
-    'README': 'FreeImage-README.txt',
-    'license-fi.txt': 'FreeImage-License.txt'
-}
+# Where to get the downloadable files
+# Note that we hardcode the filenames. In theory we could make a text file
+# that lists the most up-to-date libraries, but the current approach is
+# much easier, and its probably enough to just change the filenames to
+# the latest libs available at each release.
+BASE_ADDRESS = 'http://bitbucket.org/almarklein/imageio/downloads/'
+
+FILES = ['README.txt', 'FreeImage-License.txt']
 
 LIBRARIES = {
     ('darwin', 32): 'libfreeimage-3.15.1-osx10.6.dylib',
@@ -25,7 +27,14 @@ LIBRARIES = {
 def _download(url, dest, timeout=20):
     print('Downloading: %s' % url)
     dest_f = open(dest, 'wb')
-    remote = urlopen(url, timeout=timeout)
+    # Open connection
+    try:
+        remote = urlopen(url, timeout=timeout)
+    except IOError:
+        dest_f.close()
+        os.remove(dest)
+        raise RuntimeError('Could not find %s.' % url)
+    # Download
     try:
         shutil.copyfileobj(remote, dest_f)
     except:
@@ -34,36 +43,50 @@ def _download(url, dest, timeout=20):
         raise RuntimeError('Could not download %s to %s.' %(url, dest))
     finally:
         remote.close()
+    # Close local file
+    dest_f.flush()
     dest_f.close()
+
+
+def get_key_for_available_lib():
+    """ Get key (platform, bits) for the current system.
+    Returns None if there is no downloadable lib for this key.
+    """
+    bits = 64 if sys.maxsize > 2**32 else 32
+    key = (sys.platform, bits)
+    if key in LIBRARIES:
+        return key
+    else:
+        return None
 
 
 def retrieve_files(retrieve_all=False):
     
     if retrieve_all:
         # We want to download all files
-        files = dict(FILES)
+        files = list(FILES)
         for key, library in LIBRARIES.items():
-            files[library] = library
+            files.append(library)
     else:
         # We only want to download the one for this system
-        bits = 64 if sys.maxsize > 2**32 else 32
-        key = (sys.platform, bits)
-        if key not in LIBRARIES:
+        key = get_key_for_available_lib()
+        if key is None:
             raise RuntimeError('No precompiled FreeImage libraries are available '
-                            'for %d-bit %s systems.'%(bits, sys.platform))
+                            'for %d-bit %s systems.'%(key[1], key[0]))
         library = LIBRARIES[key]
-        print('Found: %s for %d-bit %s systems at %s' % (library, bits, 
-                sys.platform, BASE_ADDRESS))
+        print('Found: %s for %d-bit %s systems at %s' % (library, key[1], 
+                key[0], BASE_ADDRESS))
         # Select files to download
-        files = dict(FILES)
-        files[library] = library
+        files = list(FILES)
+        files.append(library)
     
     # Download all files and put them in the lib dir
-    for src, dst in files.items():
-        dest = os.path.join(os.path.dirname(__file__), 'lib', dst)
+    for fname in files:
+        src = BASE_ADDRESS+fname
+        dest = os.path.join(LOCALDIR, 'lib', fname)
         if not os.path.exists(dest):
-            _download(BASE_ADDRESS+src, dest)
+            _download(src, dest)
 
 
 if __name__ == '__main__':
-    retrieve_files()
+    retrieve_files(True)
