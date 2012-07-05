d3bf9377efb59fb7edb03e1bc82af15ff441c967
almar <devnull@localhost>
2012-07-05 23:30:06 +0200

    Major refactoring to implemented the changes proposed in https://groups.google.com/d/msg/imageio/lPMH0LJ4XEQ/IR564DuRDBgJ.

---
+++ functions.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012, imageio contributers
+# imageio is distributed under the terms of the (new) BSD License.
+
+""" imageio.functions
+
+This module defines the functions that are exposed to the user.
+
+"""
+
+
+import sys
+import os 
+import numpy as np
+
+from imageio import formats
+from imageio import base
+
+# Taken from six.py
+PY3 = sys.version_info[0] == 3
+if PY3:
+    string_types = str,
+    text_type = str
+    binary_type = bytes
+else:
+    string_types = basestring,
+    text_type = unicode
+    binary_type = str
+
+
+## Base functions that return a reader/writer
+
+def read(filename, format=None, expect=None, **kwargs):
+    """ read(filename, format=None, expect=None, **kwargs)
+    
+    Returns a reader object which can be used to read data and info 
+    from the specified file.
+    
+    Parameters
+    ----------
+    filename : str
+        The location of the file on the file system.
+    format : str
+        The format to use to read the file. By default imageio selects
+        the appropriate for you based on the filename and its contents.
+    expect : {imageio.EXPECT_IM, imageio.EXPECT_MIM, imageio.EXPECT_VOL}
+        Used to give the reader a hint on what the user expects. Optional.
+    
+    Further keyword arguments are passed to the reader. See the docstring
+    of the corresponding format to see what arguments are available.
+    
+    """ 
+    
+    # Test filename
+    if not isinstance(filename, string_types):
+        raise TypeError('Filename must be a string.')
+    if not os.path.isfile(filename):
+        raise IOError("No such file: '%s'" % filename)
+    
+    # Create request object
+    request = base.Request(filename, expect, **kwargs)
+    
+    # Get format
+    if format is not None:
+        format = formats[format]
+    else:
+        format = formats.search_read_format(request)
+    if format is None:
+        raise ValueError('Could not find a format to read the specified file.')
+    
+    # Return its reader object
+    return format.read(request)
+
+
+def save(filename, format=None, **kwargs):
+    """ save(filename, format=None, **kwargs)
+    
+    Returns a writer object which can be used to write data and info 
+    to the specified file.
+    
+    Parameters
+    ----------
+    filename : str
+        The location of the file to save to.
+    format : str
+        The format to use to read the file. By default imageio selects
+        the appropriate for you based on the filename.
+    
+    Further keyword arguments are passed to the writer. See the docstring
+    of the corresponding format to see what arguments are available.
+    
+    """ 
+    
+    # Test filename
+    if not isinstance(filename, string_types):
+        raise TypeError('Filename must be a string.')
+    
+    # Create request object (expect is None)
+    request = base.Request(filename, None, **kwargs)
+    
+    # Get format
+    if format is not None:
+        format = formats[format]
+    else:
+        format = formats.search_save_format(request)
+    if format is None:
+        raise ValueError('Could not find a format to save the specified file.')
+    
+    # Return its writer object
+    return format.save(request)
+
+
+## Images
+
+def imread(filename, format=None, **kwargs):
+    """ imread(filename, format=None, **kwargs)
+    
+    Reads an image from a specified file. Returns a numpy array.
+    
+    Parameters
+    ----------
+    filename : str
+        The location of the file on the file system.
+    format : str
+        The format to use to read the file. By default imageio selects
+        the appropriate for you based on the filename and its contents.
+    
+    Further keyword arguments are passed to the reader. See the docstring
+    of the corresponding format to see what arguments are available.
+    
+    """ 
+    
+    # Get reader and read first
+    reader = read(filename, format, base.EXPECT_IM, **kwargs)
+    with reader:
+        return reader.read_data(0)
+
+
+def imsave(filename, im, format=None, **kwargs):
+    """ imsave(filename, im, format=None, **kwargs)
+    
+    Write an image to a specified file.
+    
+    Parameters
+    ----------
+    filename : str
+        The location of the file to save to.
+    im : numpy.ndarray
+        The image data. Must be NxM, NxMx3 or NxMx4.
+    format : str
+        The format to use to read the file. By default imageio selects
+        the appropriate for you based on the filename and its contents.
+    
+    Further keyword arguments are passed to the writer. See the docstring
+    of the corresponding format to see what arguments are available.
+    
+    """ 
+    
+    # Test image
+    if isinstance(im, np.ndarray):
+        if im.ndim == 2:
+            pass
+        elif im.ndim == 3 and im.shape[2] in [3,4]:
+            pass
+        else:
+            raise ValueError('Image must be 2D (grayscale, RGB, or RGBA).')
+    else:
+        raise ValueError('Image must be a numpy array.')
+    
+    # Get writer der and write first
+    writer = save(filename, format, base.EXPECT_IM, **kwargs)
+    with writer:
+        writer.save_data(im, 0)
+
+
+## Multiple images
+
+def mimread(self, filename, ims, format, **kwargs):
+    raise NotImplemented()
+
+def mimsave(self, filename, ims, format, **kwargs):
+    raise NotImplemented()
+
+
+## Volumes
+
+def volread(self, filename, vol, format, **kwargs):
+    raise NotImplemented()
+
+
+def volsave(self, filename, vol, format, **kwargs):
+    raise NotImplemented()
+
+
+## Multiple volumes
+
+def mvolread(self, filename, vol, format, **kwargs):
+    raise NotImplemented()
+
+
+def mvolsave(self, filename, vol, format, **kwargs):
+    raise NotImplemented()
--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,6 +2,63 @@
 # Copyright (c) 2012, imageio contributers
 # imageio is distributed under the terms of the (new) BSD License.
 
-""" This just imports each plugin. 
+""" 
+
+What is a plugin
+----------------
+
+In imageio, a plugin provides one or more Format objects, and corresponding
+Reader and Writer classes.
+
+Each Format object represents an implementation to read/save a particular 
+file format. The Reader and Writer classes do the actual reading/saving.
+
+
+Registering
+-----------
+
+Strictly speaking a format can be used stand alone. However, to allow 
+imageio to automatically select it for a specific file, the format must
+be registered using imageio.formats.add_format(). 
+
+Note that a plugin is not required to be part of the imageio package; as
+long as a format is registered, imageio can use it. This makes imageio very 
+easy to extend.
+
+
+What methods to implement
+--------------------------
+
+Imageio is designed such that plugins mostly only need to implement 
+private methods. These private methods are called from public methods.
+In effect, the public methods can be given a descent docstring which
+does not have to be repeated at the plugins.
+
+For the Format class, the following needs to be implemented/specified:
+
+  * The format needs a short name, a description and a list of file extensions
+    that are common for the file-format in question.
+  * Use a docstring to provide more detailed information about the format/plugin.
+  * Set the _readerClass and _writerClass attributes. 
+  * Implement _can_read(request), return a bool. See also the Request class.
+  * Implement _can_save(request), dito.
+
+For the Reader class:
+    
+  * Implement _read_data(*indices, **kwargs)
+  * Implement _read_info(*indices, **kwargs), empty indices means global info.
+  * Implement _mshape(), should implement the shape of the data. 1 for single
+    image, 5 for 5 images (4,5) for a series of 4 images of 5 stacks, etc.
+    Under construction... we might also want to specify that we dont know
+    the number if images.
+  * _close() - close files, release resources.
+
+For the Writer class:
+    
+  * Implement _save_data(data, *indices, **kwargs)
+  * Implement _save_info(info, *indices, **kwargs), empty indices means global info.
+  * _close() - close files, release resources.
+
 """
+
 import imageio.plugins.plugin_freeimage
