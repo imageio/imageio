994ff4ef64b5b10e36866b44398c0282a96fcf32
Almar Klein <almar.klein@gmail.com>
2012-07-15 21:18:06 +0200

    Fix to allow rtd to parse the docs.

--- docs/docsinserter.py
+++ docs/docsinserter.py
@@ -11,6 +11,18 @@ imageio._format_docs = imageio.formats.create_docs_for_all_formats()
 # Insert docs
 insertdocs.parse_rst_files(NS=globals())
 
+# Set version
+text = open('conf.py','rb').read().decode('utf-8')
+lines = []
+for line in text.splitlines():
+    if line.startswith('version = '):
+        line = "version  = '%s'" % '.'.join(imageio.__version__.split('.')[:2])
+    elif line.startswith('release = '):
+        line = "release = '%s'" % imageio.__version__
+    lines.append(line)
+text = '\n'.join(lines)
+open('conf.py','wb').write(text.encode('utf-8'))
+
 # Tell Sphinx to build the docs
 if sys.platform.startswith('win'):
     p = subprocess.check_call(['make.bat', 'html'])
