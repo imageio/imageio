da74d7f689497a1dca341529d010ff43136f3074
Almar Klein <almar.klein@gmail.com>
2012-12-06 22:19:47 +0100

    Fixed freezing (on Linux). And some small fixes.

--- imageio/freeze.py
+++ imageio/freeze.py
@@ -123,6 +123,14 @@ def resource_dir(package_name, subdir=None):
 
 
 def get_includes():
+    if sys.version_info[0] == 3:
+        urllib = ['email', 'urllib.request',]
+    else:
+        urllib = ['urllib2']
+    return urllib + ['numpy', 'zipfile', 'io']
+
+
+def get_excludes():
     return []
 
 
