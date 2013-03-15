eb7a878d288a834ffebcab512bb209105772e796
Almar Klein <almar.klein@gmail.com>
2013-03-15 11:12:24 +0100

    Updated docs for dicom plugin.

--- imageio/plugins/dicom.py
+++ imageio/plugins/dicom.py
@@ -5,6 +5,13 @@
 """ Plugin for reading DICOM files.
 """
 
+# todo: Use pydicom:
+# * Note: is not py3k ready yet
+# * Allow reading the full meta info
+# * I think we can more or less replace the SimpleDicomReader with a pydicom.Dataset
+# * For series, only ned to read the full info from one file: speed still high
+# * Perhaps allow writing?
+
 import sys
 import os
 import struct
@@ -38,6 +45,14 @@ class DicomFormat(Format):
     """ A format for reading DICOM images: a common format used to store
     medical image data, such as X-ray, CT and MRI.
     
+    This format borrows some code (and ideas) from the pydicom project,
+    and (to the best of our knowledge) has the same limitations as
+    pydicom with regard to the type of files that it can handle. However,
+    only a predefined subset of tags are extracted from the file. This allows
+    for great simplifications allowing us to make a stand-alone reader, and
+    also results in a much faster read time. We plan to allow reading all
+    tags in the future (by using pydicom).
+    
     Keyword arguments for reading
     -----------------------------
     progress : {True, False, BaseProgressIndicator}
@@ -52,7 +67,7 @@ class DicomFormat(Format):
         return request.firstbytes[128:132] == b'DICM'
     
     def _can_save(self, request):
-        # We cannot save yet. Will be possible if we will used pydicom as
+        # We cannot save yet. May be possible if we will used pydicom as
         # a backend.
         return False
     
@@ -718,10 +733,6 @@ class SimpleDicomReader(object):
         unzipped = zlib.decompress(zipped, -zlib.MAX_WBITS)
         self._file = BytesIO(unzipped)  # a file-like object
 
-
-
-# todo: with some modifications, the SimpleDicomReader can be replaced by a pydicom dataset
-# ... allowing us to real *all* tags
 
 
 class DicomSeries(object):
