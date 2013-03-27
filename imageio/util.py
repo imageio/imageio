1bc4d393e98a0cbf1c2a4d01ab13d4111fc25498
a <devnull@localhost>
2013-03-27 12:15:00 +0100

    Changed the __repr__ of the Image class. It now explicitly says it is a numpy array and also mentions the dtype.

--- imageio/util.py
+++ imageio/util.py
@@ -81,10 +81,11 @@ class Image(np.ndarray):
     
     def __repr__(self):
         n = 'x'.join([str(i) for i in self.shape])
+        dtype = self.dtype
         ndim = self.ndim
         if self.shape[-1] in (1,3,4):
             ndim -= 1
-        return '<%iD image of %s elements>' % (ndim, n)
+        return '<%iD image: numpy array with %s elements of dtype %s>' % (ndim, n, dtype)
     
     def __str__(self):
         return np.ndarray.__str__(self) # print() shows elements as normal
