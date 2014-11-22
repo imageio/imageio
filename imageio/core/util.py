4d64113bec6a2b00e181ee921c3c723a4db88bc4
Almar Klein <almar.klein@gmail.com>
2014-11-22 23:43:49 +0100

    Allow resources inside the package structure
    We keep a special directory for local resources.
    Then we can create wheels for distribution that have
    the FreeImage lib and a couple of images inside this
    directory.

--- imageio/core/util.py
+++ imageio/core/util.py
@@ -17,6 +17,7 @@ import struct
 import numpy as np
 
 IS_PYPY = '__pypy__' in sys.builtin_module_names
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Taken from six.py
 PY3 = sys.version_info[0] == 3
@@ -444,8 +445,31 @@ def appdata_dir(appname=None, roaming=False):
     
     # Done
     return path
-   
+
+
+def resource_dirs():
+    """ resource_dirs()
     
+    Get a list of directories where imageio resources may be located.
+    The first directory in this list is the "resources" directory in
+    the package itself. The list further contains the application
+    directory (for frozen apps), and may include additional directories
+    in the future.
+    """
+    dirs = []
+    # Resource dir baked in the package
+    dirs.append(os.path.abspath(os.path.join(THIS_DIR, '..', 'resources')))
+    # Directory where the app is located (mainly for frozen apps)
+    if sys.path and sys.path[0]:
+        # Get the path. If frozen, sys.path[0] is the name of the executable,
+        # otherwise it is the path to the directory that contains the script.
+        thepath = sys.path[0]
+        if getattr(sys, 'frozen', None):
+            thepath = os.path.dirname(thepath)
+        dirs.append(os.path.abspath(thepath))
+    return dirs
+
+
 def get_platform():
     """ get_platform()
     
