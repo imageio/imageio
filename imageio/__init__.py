3db112824c336f9b3336149fcc44359e58642a88
Almar Klein <almar.klein@gmail.com>
2015-02-04 14:25:11 +0100

    This is v1.1

--- imageio/__init__.py
+++ imageio/__init__.py
@@ -14,7 +14,7 @@ and 3.x, and is easy to install.
 Main website: http://imageio.github.io
 """
 
-__version__ = '1.0dev' 
+__version__ = '1.1' 
 
 # Load some bits from core
 from .core import FormatManager, RETURN_BYTES  # noqa
