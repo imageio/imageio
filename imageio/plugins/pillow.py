51f4cfb45274e20619683d4823e034d1870bb015
Almar Klein <almar.klein@gmail.com>
2016-10-13 02:27:17 +0200

    Pillow plugin works for non-sequence images

--- imageio/plugins/pillow.py
+++ imageio/plugins/pillow.py
@@ -12,68 +12,20 @@ import numpy as np
 from .. import formats
 from ..core import Format, image_as_uint
 
-try:
-    import PIL
-    if not hasattr(PIL, 'PILLOW_VERSION'):
-        raise ImportError('Imageio Pillow plugin needs Pillow, not PIL!')
-except ImportError as err:
-    raise
+# Get info about pillow formats without having to import PIL
+from .pillow_info import pillow_formats, pillow_docs
 
-from PIL import Image
-
-# Image.ID - list of plugin id's
-# Image.OPEN - dict of id: (factory, accept)
-# Image.SAVE - dict of id: (write) ?
-# Image.EXTENSION - dict of ext: id
-
-# Get docs: https://raw.githubusercontent.com/python-pillow/Pillow/master/docs/handbook/image-file-formats.rst
-
-FULL_FORMATS = set(Image.ID).intersection(Image.SAVE.keys())
-READABLE_FORMATS = set(Image.ID).intersection(Image.OPEN.keys()).difference(FULL_FORMATS)
-
-
-FULL_FORMATS = ('.bmp', '.eps', '.gif', '.icns', '.im',
-                '.jpeg', '.jpg', '.jpeg2000',
-                '.msp', '.pcx', '.png', '.ppm', '.spider', '.tiff', '.webp',
-                '.xbm')
-
-READABLE_FORMATS = ('.cur', '.dcx', '.dds', '.fli', '.flc', '.fpx', '.gbr',
-                    '.gd', '.ico', '.icns', '.imt', '.iptc', '.naa', '.mcidas',
-                    '.mpo', '.pcd', '.psd', '.cgi', '.tga', '.wal', '.xpm')
-
-WRITABLE_FORMATS = ('.palm', '.pdf', '.pixar')
-
-# Pillow "knows" these, without being able to read or write them
-IDENTIFIABLE_FORMATS = ('.bufr', '.fits', '.grib', '.hdf5', '.mpeg', '.wmf')
-
-
-# todo: Pillow ImageGrab module supports grabbing the screen on Win and OSX. how awesome it that!
-
-# todo: also ImageSequence module
-
-# todo: format has short name by default, unless it already exists,
-# in which case it gets a "PIL_" prefix. Make possible to use that prefix
-# even for the formats that got the prefix-less name.
-
+# todo: Pillow ImageGrab module supports grabbing the screen on Win and OSX.
+# todo: also ImageSequence module -> GIF
 
 
 class PillowFormat(Format):
-    """ The dummy format is an example format that does nothing.
-    It will never indicate that it can read or write a file. When
-    explicitly asked to read, it will simply read the bytes. When 
-    explicitly asked to write, it will raise an error.
-    
-    This documentation is shown when the user does ``help('thisformat')``.
-    
-    Parameters for reading
-    ----------------------
-    Specify arguments in numpy doc style here.
-    
-    Parameters for saving
-    ---------------------
-    Specify arguments in numpy doc style here.
-    
     """
+    Base format class for Pillow formats.
+    """
+    
+    _pillow_imported = False
+    _Image = None
     
     @property
     def plugin_id(self):
@@ -82,14 +34,25 @@ class PillowFormat(Format):
         return self._plugin_id  # Set when format is created
     
     def _init_pillow(self):
-        # Init Pillow
+        if not self._pillow_imported:
+            self._pillow_imported = True  # more like tried to import
+            import PIL
+            if not hasattr(PIL, 'PILLOW_VERSION'):
+                raise ImportError('Imageio Pillow plugin needs Pillow, not PIL!')
+            from PIL import Image
+            self._Image = Image
+        elif self._Image is None:
+            raise RuntimeError('Imageio Pillow plugin cannot work without Pillow.')
+        
+        Image = self._Image
         if self.plugin_id in ('PNG', 'JPEG', 'BMP', 'GIF', 'PPM'):
             Image.preinit()
         else:
             Image.init()
+        return Image
     
     def _can_read(self, request):
-        self._init_pillow()
+        Image = self._init_pillow()
         if request.mode[1] in (self.modes + '?'):
             if self.plugin_id in Image.OPEN:
                 factory, accept = Image.OPEN[self.plugin_id]
@@ -97,18 +60,17 @@ class PillowFormat(Format):
                     return True
     
     def _can_write(self, request):
-        self._init_pillow()
+        Image = self._init_pillow()
         if request.mode[1] in (self.modes + '?'):
             if request.filename.lower().endswith(self.extensions):
                 if self.plugin_id in Image.SAVE:
                     return True
     
-    # -- reader
     
     class Reader(Format.Reader):
     
         def _open(self, **kwargs):
-            self.format._init_pillow()
+            Image = self.format._init_pillow()
             try:
                 factory, accept = Image.OPEN[self.format.plugin_id]
             except KeyError:
@@ -134,12 +96,11 @@ class PillowFormat(Format):
         def _get_meta_data(self, index):
             return self._im.info
     
-    # -- writer
     
     class Writer(Format.Writer):
         
         def _open(self, **kwargs):   
-            self.format._init_pillow()
+            Image = self.format._init_pillow()
             try:
                 self._save_func = Image.SAVE[self.format.plugin_id]
             except KeyError:
@@ -159,7 +120,7 @@ class PillowFormat(Format):
                                    self.format.name)
             self._written = True
             self._meta.update(meta)
-            im = Image.fromarray(im)
+            im = self.format._Image.fromarray(im)
             im.encoderinfo = self._meta
             self._save_func(im, self._fp, '')
             im.close()
@@ -168,15 +129,13 @@ class PillowFormat(Format):
             self._meta.update(meta)
 
 
-pngformat = PillowFormat('PNG-PIL', "Portable network graphics", '.png', 'i')
-pngformat._plugin_id = 'PNG'
-pngformat.__doc__ = 'blabla docs for png'
-formats.add_format(pngformat)
+def register_pillow_formats():
+    
+    for id, summary, ext in pillow_formats:
+        format = PillowFormat(id + '-PIL', summary, ext, 'i')
+        format._plugin_id = id
+        format.__doc__ = pillow_docs[id]
+        formats.add_format(format)
 
-# # Register. You register an *instance* of a Format class. Here specify:
-# format = DummyFormat('dummy',  # short name
-#                      'An example format that does nothing.',  # one line descr.
-#                      '.foobar .nonexistentext',  # list of extensions
-#                      'iI'  # modes, characters in iIvV
-#                      )
-# formats.add_format(format)
+
+register_pillow_formats()
