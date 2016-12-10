2d7f407c0c299d1b35d92f56fb63c1746ce740b7
Almar Klein <almar.klein@gmail.com>
2016-12-10 13:34:01 +0100

    version to dev

--- imageio/__init__.py
+++ imageio/__init__.py
@@ -16,7 +16,7 @@ Main website: http://imageio.github.io
 
 # flake8: noqa
 
-__version__ = '2.0.0'
+__version__ = '2.0.0dev'
 
 # Load some bits from core
 from .core import FormatManager, RETURN_BYTES
