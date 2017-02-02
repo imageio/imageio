99cc1e56d407b9d90ca4a86a664bb9d86117bf17
Almar Klein <almar.klein@gmail.com>
2017-02-02 12:10:59 +0100

    bunb version to dev

--- imageio/__init__.py
+++ imageio/__init__.py
@@ -16,7 +16,7 @@ Main website: http://imageio.github.io
 
 # flake8: noqa
 
-__version__ = '2.1.2'
+__version__ = '2.1.2dev'
 
 # Load some bits from core
 from .core import FormatManager, RETURN_BYTES
