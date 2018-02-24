45db22e813dbf8d5270c99d645967a83d4218564
Almar Klein <almar.klein@gmail.com>
2018-02-24 01:28:15 +0100

    fix date, it has not been quite *that* long.

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'imageio'
-copyright = u'1014-2018, imageio contributors'
+copyright = u'2014-2018, imageio contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
