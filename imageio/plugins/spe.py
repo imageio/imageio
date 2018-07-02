20c2c1750964af7c3ad5306796fd80b43c4523cb
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-07-02 11:31:36 +0200

    Add plugin for reading SPE files

---
+++ imageio/plugins/spe.py
@@ -0,0 +1,281 @@
+import os
+import warnings
+import numpy as np
+
+from .. import formats
+from ..core import Format
+
+
+class Spec:
+    """SPE file specification data
+
+    Tuples of (offset, datatype, count), where offset is the offset in the SPE
+    file and datatype is the datatype as used in `numpy.fromfile`()
+
+    `data_start` is the offset of actual image data.
+
+    `dtypes` translates SPE datatypes (0...4) to numpy ones, e. g. dtypes[0]
+    is dtype("<f") (which is np.float32).
+
+    `controllers` maps the `type` metadata to a human readable name
+
+    `readout_modes` maps the `readoutMode` metadata to something human readable
+    although this may not be accurate since there is next to no documentation
+    to be found.
+    """
+    basic = {
+        "datatype": (108, "<h"), #dtypes
+        "xdim": (42, "<H"),
+        "ydim": (656, "<H"),
+        "NumFrames": (1446, "<i"),
+    }
+
+    metadata = {
+        # ROI information
+        "NumROI": (1510, "<h"),
+        "ROIs": (1512, np.dtype([("startx", "<H"),
+                                 ("endx", "<H"),
+                                 ("groupx", "<H"),
+                                 ("starty", "<H"),
+                                 ("endy", "<H"),
+                                 ("groupy", "<H")]), 10),
+
+        # chip-related sizes
+        "xDimDet": (6, "<H"),
+        "yDimDet": (18, "<H"),
+        "VChipXdim": (14, "<h"),
+        "VChipYdim": (16, "<h"),
+
+        # other stuff
+        "ControllerVersion": (0, "<h"),
+        "LogicOutput": (2, "<h"),
+        "AmpHiCapLowNoise": (4, "<H"), # enum?
+        "mode": (8, "<h"), # enum?
+        "exp_sec": (10, "<f"),
+        "date": (20, "<10S"),
+        "DetTemperature": (36, "<f"),
+        "DetType": (40, "<h"),
+        "stdiode": (44, "<h"),
+        "DelayTime": (46, "<f"),
+        "ShutterControl": (50, "<H"), # normal, disabled open, disabled closed
+                                      # but which one is which?
+        "AbsorbLive": (52, "<h"), # bool?
+        "AbsorbMode": (54, "<H"),
+        "CanDoVirtualChipFlag": (56, "<h"), # bool?
+        "ThresholdMinLive": (58, "<h"), # bool?
+        "ThresholdMinVal": (60, "<f"),
+        "ThresholdMinLive": (64, "<h"), # bool?
+        "ThresholdMinVal": (66, "<f"),
+        "ExperimentTimeLocal": (172, "<7S"),
+        "ExperimentTimeUTC": (179, "<7S"),
+        "ADCoffset": (188, "<H"),
+        "ADCrate": (190, "<H"),
+        "ADCtype": (192, "<H"),
+        "ADCresolution": (194, "<H"),
+        "ADCbitAdjust": (196, "<H"),
+        "gain": (198, "<H"),
+        "comments": (200, "<80S", 5),
+        "geometric": (600, "<H"), # flags
+        "swversion": (688, "<16S"),
+        "spare4": (742, "<436S"),
+        "XPrePixels": (98, "<h"),
+        "XPostPixels": (100, "<h"),
+        "YPrePixels": (102, "<h"),
+        "YPostPixels": (104, "<h"),
+        "ReadoutTime": (672, "<f"),
+        "type": (704, "<h"), # controllers
+        "clkspd_us": (1428, "<f"),
+        "readoutMode": (1480, "<H"), # readout_modes
+        "WindowSize": (1482, "<H"),
+        "file_header_ver": (1992, "<f")
+    }
+
+    data_start = 4100
+
+    dtypes = [np.dtype("<f"), np.dtype("<i"), np.dtype("<h"),
+              np.dtype("<H"), np.dtype("<I")]
+
+    controllers = [
+        "new120 (Type II)", "old120 (Type I)", "ST130", "ST121", "ST138",
+        "DC131 (PentaMax)", "ST133 (MicroMax/Roper)", "ST135 (GPIB)", "VTCCD",
+        "ST116 (GPIB)", "OMA3 (GPIB)", "OMA4"
+    ]
+
+    # This was gathered from random places on the internet and own experiments
+    # with the camera. May not be accurate.
+    readout_modes = ["full frame", "frame transfer", "kinetics"]
+
+    # Do not decode the following metadata keys into strings, but leave them
+    # as byte arrays
+    no_decode = ["spare4"]
+
+
+class SpeFormat(Format):
+    def _can_read(self, request):
+        return (request.mode[1] in self.modes + "?" and
+                request.extension in self.extensions)
+
+    def _can_write(self, request):
+        return False
+
+    class Reader(Format.Reader):
+        def _open(self, char_encoding="latin1", check_filesize=True):
+            self._file = self.request.get_file()
+            self._char_encoding = char_encoding
+
+            info = self._parse_header(Spec.basic)
+            self._dtype = Spec.dtypes[info["datatype"]]
+            self._shape = (info["ydim"], info["xdim"])
+            self._len = info["NumFrames"]
+
+            if check_filesize:
+                # Some software writes incorrecet `NumFrames` metadata
+                # Use the file size to determine the number of frames
+                fsz = os.path.getsize(self.request.get_local_filename())
+                l = fsz - Spec.data_start
+                l //= self._shape[0] * self._shape[1] * self._dtype.itemsize
+                if l != self._len:
+                    warnings.warn("Number of frames according to file header "
+                                  "does not match the size of file " +
+                                  filename + ".")
+                    self._len = min(l, self._len)
+
+            self._meta = None
+
+        def _get_meta_data(self, index):
+            if self._meta is None:
+                self._meta = self._parse_header(Spec.metadata)
+
+                nr = self._meta.pop("NumROI", None)
+                nr = 1 if nr < 1 else nr
+                self._meta["ROIs"] = roi_array_to_dict(self._meta["ROIs"][:nr])
+
+                # chip sizes
+                self._meta["ChipSize"] = [self._meta.pop("xDimDet", None),
+                                          self._meta.pop("yDimDet", None)]
+                self._meta["VirtChipSize"] = [
+                    self._meta.pop("VChipXdim", None),
+                    self._meta.pop("VChipYdim", None)]
+                self._meta["PrePixels"] = [self._meta.pop("XPrePixels", None),
+                                           self._meta.pop("YPrePixels", None)]
+                self._meta["PostPixels"] = [
+                    self._meta.pop("XPostPixels", None),
+                    self._meta.pop("YPostPixels", None)]
+
+                # comments
+                self._meta["comments"] = [str(c)
+                                          for c in self._meta["comments"]]
+
+                # geometric operations
+                g = []
+                f = self._meta.pop("geometric", 0)
+                if f & 1:
+                    g.append("rotate")
+                if f & 2:
+                    g.append("reverse")
+                if f & 4:
+                    g.append("flip")
+                self._meta["geometric"] = g
+
+                #Make some additional information more human-readable
+                t = self._meta["type"]
+                if 1 <= t <= len(Spec.controllers):
+                    self._meta["type"] = Spec.controllers[t - 1]
+                else:
+                    self._meta.pop("type", None)
+                m = self._meta["readoutMode"]
+                if 1 <= m <= len(Spec.readout_modes):
+                    self._meta["readoutMode"] = Spec.readout_modes[m - 1]
+                else:
+                    self._meta.pop("readoutMode", None)
+            return self._meta
+
+        def _close(self):
+            # The file should be closed by `self.request`
+            pass
+
+        def _parse_header(self, spec):
+            ret = {}
+            # Decode each string from the numpy array read by np.fromfile
+            decode = np.vectorize(lambda x: x.decode(self._char_encoding))
+
+            for name, sp in spec.items():
+                self._file.seek(sp[0])
+                cnt = (1 if len(sp) < 3 else sp[2])
+                v = np.fromfile(self._file, dtype=sp[1], count=cnt)
+                if v.dtype.kind == "S" and name not in Spec.no_decode:
+                    # Silently ignore string decoding failures
+                    try:
+                        v = decode(v)
+                    except:
+                        warnings.warn("Failed to decode \"{}\" metadata "
+                                      "string. Check `char_encoding` "
+                                      "parameter.".format(name))
+
+                try:
+                    # For convenience, if the array contains only one single
+                    # entry, return this entry itself.
+                    v = np.asscalar(v)
+                except ValueError:
+                    v = np.ravel(v)
+                ret[name] = v
+            return ret
+
+        def _get_length(self):
+            if self.request.mode[1] in 'vV':
+                return 1
+            else:
+                return self._len
+
+        def _get_data(self, index):
+            if index < 0:
+                raise IndexError("Image index %i < 0" % index)
+            if index >= self._len:
+                raise IndexError("Image index %i > %i" % (index, self._len))
+
+            if self.request.mode[1] in "vV":
+                if index != 0:
+                    raise IndexError("Index has to be 0 in v and V modes")
+                self._file.seek(Spec.data_start)
+                data = np.fromfile(
+                    self._file, dtype=self._dtype,
+                    count=self._shape[0] * self._shape[1] * self._len)
+                data = data.reshape((self._len,) + self._shape)
+            else:
+                self._file.seek(
+                    Spec.data_start + index * self._shape[0] *
+                    self._shape[1] * self._dtype.itemsize)
+                data = np.fromfile(
+                    self._file, dtype=self._dtype,
+                    count=self._shape[0] * self._shape[1])
+                data = data.reshape(self._shape)
+            return data, self._get_meta_data(index)
+
+
+def roi_array_to_dict(a):
+    """Convert the `ROIs` structured arrays to :py:class:`dict`
+
+    Parameters
+    ----------
+    a : numpy.ndarray
+        Structured array containing ROI data
+
+    Returns
+    -------
+    list of dict
+        One dict per ROI. Keys are "top_left", "bottom_right", and "bin",
+        values are tuples whose first element is the x axis value and the
+        second element is the y axis value.
+    """
+    l = []
+    a = a[["startx", "starty", "endx", "endy", "groupx", "groupy"]]
+    for sx, sy, ex, ey, gx, gy in a:
+        d = {"top_left": [int(sx), int(sy)],
+             "bottom_right": [int(ex), int(ey)],
+             "bin": [int(gx), int(gy)]}
+        l.append(d)
+    return l
+
+
+fmt = SpeFormat("spe", "SPE file format", ".spe", "iIvV")
+formats.add_format(fmt, overwrite=True)
