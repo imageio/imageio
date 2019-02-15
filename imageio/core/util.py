01e3a90026c8b9043aa487fb41da52e462491b26
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2019-02-15 20:04:50 +0000

    Add preliminary FreeBSD support to fix moviepy on FreeBSD
    Reference:	https://svnweb.freebsd.org/changeset/ports/475820

--- imageio/core/util.py
+++ imageio/core/util.py
@@ -550,6 +550,8 @@ def get_platform():
         plat = "win%i"
     elif sys.platform.startswith("darwin"):
         plat = "osx%i"
+    elif sys.platform.startswith("freebsd"):
+        plat = "freebsd%i"
     else:  # pragma: no cover
         return None
 
