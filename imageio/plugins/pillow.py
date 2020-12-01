b3c8bdb8956507b5fe13fc2565ff4c3c1abfe6b6
Benedikt Lorch <benedikt.lorch@fau.de>
2020-12-01 12:42:36 +0100

    Allow quality factor to be 0. Update comment.

--- imageio/plugins/pillow.py
+++ imageio/plugins/pillow.py
@@ -481,10 +481,10 @@ class JPEGFormat(PillowFormat):
     class Writer(PillowFormat.Writer):
         def _open(self, quality=75, progressive=False, optimize=False, **kwargs):
 
-            # Check quality - in Pillow it should be no higher than 95
+            # The JPEG quality factor can be between 0 (worst) and 100 (best)
             quality = int(quality)
-            if quality < 1 or quality > 100:
-                raise ValueError("JPEG quality should be between 1 and 100.")
+            if quality < 0 or quality > 100:
+                raise ValueError("JPEG quality should be between 0 and 100.")
 
             kwargs["quality"] = quality
             kwargs["progressive"] = bool(progressive)
