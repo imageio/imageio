58435718a04aa8863f0a26740df6165c5797e244
FirefoxMetzger <sebastian@wallkoetter.net>
2021-02-11 13:33:18 +0100

    proper plumbing

--- imageio/core/functional_api.py
+++ imageio/core/functional_api.py
@@ -2,7 +2,8 @@ from .imopen import imopen
 import numpy as np
 
 
-def imread(uri, *args, index=None, plugin=None, **kwargs):
+def imread(uri, *args, index=None, plugin=None, legacy_api=True,
+           extra_kwargs_plugin=None, **extra_kwargs_read):
     """ Read an ndimage from a URI.
 
     Opens the given URI and reads an ndimage from it. The exact behavior
@@ -23,24 +24,38 @@ def imread(uri, *args, index=None, plugin=None, **kwargs):
         from among them and return it. The exact behavior is plugin dependent.
     plugin : {str, None}
         The plugin to be used. If None, performs a search for a matching plugin.
-    **kwargs :
-        Additional keyword arguments will be passed to the plugin instance.
+    legacy_api : {bool}
+        The API to be used. If true use the v2.9 API, otherwise use the new
+        v3.0 API.
+    extra_kwargs_plugin : {dict, None}
+        Additional keyword arguments to pass to the plugin's constructor.
+    **extra_kwargs_read :
+        Additional keyword arguments will be passed to the plugin's read call.
 
     Returns
     -------
     image : ndimage
     """
 
-    with imopen()(uri, *args, plugin=plugin, **kwargs) as img_file:
-        return np.asarray(img_file.read(index=index))
+    plugin_kwargs = {
+        "legacy_api": legacy_api,
+        "plugin": plugin
+    }
+
+    if extra_kwargs_plugin is not None:
+        plugin_kwargs.update(extra_kwargs_plugin)
+
+    with imopen()(uri, *args, **plugin_kwargs) as img_file:
+        return np.asarray(img_file.read(index=index, **extra_kwargs_read))
 
 
-def imiter(uri, *args, plugin=None, **kwargs):
+def imiter(uri, *args, plugin=None, legacy_api=True,
+           extra_kwargs_plugin=None, **extra_kwargs_iter):
     """ Read a sequence of ndimages from a URI.
 
     Returns an iterable that yields ndimages from the given URI. The exact
-    behavior depends on both, the file type and plugin used to open the file. To
-    learn about the exact behavior, check the documentation of the relevant
+    behavior depends on both, the file type and plugin used to open the file.
+    To learn about the exact behavior, check the documentation of the relevant
     plugin.
 
     Parameters
@@ -52,15 +67,32 @@ def imiter(uri, *args, plugin=None, **kwargs):
         Additional positional arguments will be passed to the plugin instance
         upon object creation.
     plugin : {str, None}
-        The plugin to be used. If None, performs a search for a matching plugin.
-    **kwargs :
-        Additional keyword arguments will be passed to the plugin instance.
+        The plugin to be used. If None, performs a search for a matching
+        plugin.
+    legacy_api : {bool}
+        The API to be used. If true use the v2.9 API, otherwise use the new
+        v3.0 API.
+    extra_kwargs_plugin : {dict, None}
+        Additional keyword arguments to pass to the plugin's constructor.
+    **extra_kwargs_iter :
+        Additional keyword arguments will be passed to the plugin's ``iter``
+        call.
 
     Returns
     -------
     image : ndimage
     """
 
-    with imopen()(uri, *args, plugin=plugin, **kwargs) as img_file:
-        for image in img_file.iter():
+    plugin_kwargs = {
+        "legacy_api": legacy_api,
+        "plugin": plugin
+    }
+
+    if extra_kwargs_plugin is not None:
+        plugin_kwargs.update(extra_kwargs_plugin)
+
+    with imopen()(uri, *args, **plugin_kwargs) as img_file:
+        for image in img_file.iter(**extra_kwargs_iter):
+            # Note: casting to ndarray here to ensure compatibility
+            # with the v2.9 API
             yield np.asarray(image)
