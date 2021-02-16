afd4e6d09ed07433749a4e1aefb1a5e0ea2a9bf6
Almar Klein <almar@almarklein.org>
2021-02-16 21:33:59 +0100

    Some of our code has become incompatible with py35, but I guess thats ok

--- setup.py
+++ setup.py
@@ -226,9 +226,9 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
