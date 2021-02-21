b0a20de58e07dfe6f09b1dbae5f45a4f963de595
FirefoxMetzger <sebastian@wallkoetter.net>
2021-02-21 16:55:40 +0100

    bugfix #600
    Removed the reader stub (use the base class reader instead). Also added a test that explicitly uses
    `pilmode` to check if it is avaiable when using gif.

--- tests/test_pillow.py
+++ tests/test_pillow.py
@@ -258,6 +258,13 @@ def test_gif():
     raises(TypeError, imageio.imread, fname, notavalidkwarg=True)
     raises(TypeError, imageio.imsave, fnamebase + "1.gif", im, notavalidk=True)
 
+def test_gif_pilmode():
+    # Bug found in issue #600
+    image = np.asarray(imageio.mimread("imageio:newtonscradle.gif", pilmode="RGB"))
+    assert np.array_equal(image.shape, (36, 150, 200, 3))
+
+    image = np.asarray(imageio.mimread("imageio:newtonscradle.gif", pilmode="RGBA"))
+    assert np.array_equal(image.shape, (36, 150, 200, 4))
 
 def test_animated_gif():
 
