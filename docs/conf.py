a02334572320d8174880efa91c13e556afb6bdc4
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-06-16 19:20:42 +0200

    BUG: add pydata theme to requirements

--- docs/conf.py
+++ docs/conf.py
@@ -123,8 +123,8 @@ html_theme_options = {
             "name": "Pipy",
             "url": "https://pypi.org/project/imageio/",
             "icon": "fas fa-box",
-        }
-    ]
+        },
+    ],
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -149,7 +149,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
