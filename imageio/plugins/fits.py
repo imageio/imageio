741cc129c87ba1180b77ec2d9a308a2b6dc1fda7
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-07-12 13:43:07 +0200

    DOC: refactor FITS

--- imageio/plugins/fits.py
+++ imageio/plugins/fits.py
@@ -1,7 +1,62 @@
 # -*- coding: utf-8 -*-
 # imageio is distributed under the terms of the (new) BSD License.
 
-""" Read/Write FITS files.
+""" Read FITS files.
+
+Backend Library: `Astropy <https://www.astropy.org/>`_
+
+.. note::
+    To use this plugin you have to install its backend::
+
+        pip install imageio[fits]
+
+Flexible Image Transport System (FITS) is an open standard defining a
+digital file format useful for storage, transmission and processing of
+scientific and other images. FITS is the most commonly used digital
+file format in astronomy.
+
+
+Parameters
+----------
+cache : bool
+    If the file name is a URL, `~astropy.utils.data.download_file` is used
+    to open the file.  This specifies whether or not to save the file
+    locally in Astropy's download cache (default: `True`).
+uint : bool
+    Interpret signed integer data where ``BZERO`` is the
+    central value and ``BSCALE == 1`` as unsigned integer
+    data.  For example, ``int16`` data with ``BZERO = 32768``
+    and ``BSCALE = 1`` would be treated as ``uint16`` data.
+
+    Note, for backward compatibility, the kwarg **uint16** may
+    be used instead.  The kwarg was renamed when support was
+    added for integers of any size.
+ignore_missing_end : bool
+    Do not issue an exception when opening a file that is
+    missing an ``END`` card in the last header.
+checksum : bool or str
+    If `True`, verifies that both ``DATASUM`` and
+    ``CHECKSUM`` card values (when present in the HDU header)
+    match the header and data of all HDU's in the file.  Updates to a
+    file that already has a checksum will preserve and update the
+    existing checksums unless this argument is given a value of
+    'remove', in which case the CHECKSUM and DATASUM values are not
+    checked, and are removed when saving changes to the file.
+disable_image_compression : bool, optional
+    If `True`, treats compressed image HDU's like normal
+    binary table HDU's.
+do_not_scale_image_data : bool
+    If `True`, image data is not scaled using BSCALE/BZERO values
+    when read.
+ignore_blank : bool
+    If `True`, the BLANK keyword is ignored if present.
+scale_back : bool
+    If `True`, when saving changes to a file that contained scaled
+    image data, restore the data to the original type and reapply the
+    original BSCALE/BZERO values.  This could lead to loss of accuracy
+    if scaling back to integer values after performing floating point
+    operations on the data.
+
 """
 
 from .. import formats
@@ -24,54 +79,7 @@ def load_lib():
 
 
 class FitsFormat(Format):
-
-    """Flexible Image Transport System (FITS) is an open standard defining a
-    digital file format useful for storage, transmission and processing of
-    scientific and other images. FITS is the most commonly used digital
-    file format in astronomy.
-
-    This format requires the ``astropy`` package.
-
-    Parameters for reading
-    ----------------------
-    cache : bool
-        If the file name is a URL, `~astropy.utils.data.download_file` is used
-        to open the file.  This specifies whether or not to save the file
-        locally in Astropy's download cache (default: `True`).
-    uint : bool
-        Interpret signed integer data where ``BZERO`` is the
-        central value and ``BSCALE == 1`` as unsigned integer
-        data.  For example, ``int16`` data with ``BZERO = 32768``
-        and ``BSCALE = 1`` would be treated as ``uint16`` data.
-
-        Note, for backward compatibility, the kwarg **uint16** may
-        be used instead.  The kwarg was renamed when support was
-        added for integers of any size.
-    ignore_missing_end : bool
-        Do not issue an exception when opening a file that is
-        missing an ``END`` card in the last header.
-    checksum : bool or str
-        If `True`, verifies that both ``DATASUM`` and
-        ``CHECKSUM`` card values (when present in the HDU header)
-        match the header and data of all HDU's in the file.  Updates to a
-        file that already has a checksum will preserve and update the
-        existing checksums unless this argument is given a value of
-        'remove', in which case the CHECKSUM and DATASUM values are not
-        checked, and are removed when saving changes to the file.
-    disable_image_compression : bool, optional
-        If `True`, treats compressed image HDU's like normal
-        binary table HDU's.
-    do_not_scale_image_data : bool
-        If `True`, image data is not scaled using BSCALE/BZERO values
-        when read.
-    ignore_blank : bool
-        If `True`, the BLANK keyword is ignored if present.
-    scale_back : bool
-        If `True`, when saving changes to a file that contained scaled
-        image data, restore the data to the original type and reapply the
-        original BSCALE/BZERO values.  This could lead to loss of accuracy
-        if scaling back to integer values after performing floating point
-        operations on the data.
+    """ See :mod:`imageio.plugins.fits`
     """
 
     def _can_read(self, request):
