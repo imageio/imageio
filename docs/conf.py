e2622339612774cb5874937c15d3af6bc4927ba1
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-08-30 15:46:06 +0200

    MAINT: still figuring out RTD

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,12 @@ import sys
 import os
 import imageio
 
+print(dir(imageio))
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration -----------------------------------------------------
 
--- docs/imageio_ext.py
+++ docs/imageio_ext.py
@@ -52,7 +52,6 @@ def prepare_reader_and_witer():
 
 def rstjinja(app, docname, source):
     if docname == "supported_formats":
-        print(dir(imageio))
         from imageio.config.extensions import known_extensions
         from imageio.config.plugins import known_plugins
 
