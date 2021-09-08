f4c2e74f45c261c41e50ef97ca201b8239386ff7
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-09-07 23:38:38 -0700

    MAINT: undo previous commit
    (it should have been a PR not a commit, but I clicked the wrong button)

--- setup.py
+++ setup.py
@@ -170,9 +170,7 @@ class build_with_images(sdist):
 
 
 # avoid pillow 8.3 because it doesn't respect numpy API
-# pinned to > 8.3.2 due to security vulnerability
-# See: https://github.com/advisories/GHSA-98vv-pw6r-q6q4
-install_requires = ["numpy", "pillow >= 8.3.2"]
+install_requires = ["numpy", "pillow != 8.3.0"]
 
 extras_require = {
     "linting": ["black", "flake8"],
