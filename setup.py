23725529bb6287ffa36cba1b9ea379936412fc9d
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-10-20 13:38:35 +0200

    BUG: create build dependencies

--- setup.py
+++ setup.py
@@ -173,6 +173,7 @@ class build_with_images(sdist):
 install_requires = ["numpy", "pillow != 8.3.0"]
 
 extras_require = {
+    "build": ["wheel"],
     "linting": ["black", "flake8"],
     "test": ["invoke", "pytest", "pytest-cov"],
     "docs": ["sphinx", "numpydoc", "pydata-sphinx-theme"],
