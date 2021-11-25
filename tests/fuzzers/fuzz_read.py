4d0a85d9f2dfa3faae2c61b07b712fd057d23593
DavidKorczynski <david@adalogics.com>
2021-11-25 13:51:39 +0000

    Fix fuzzer build
    Fixes fuzzer coverage instrumentation following upgrades in the Atheris fuzzer

--- tests/fuzzers/fuzz_read.py
+++ tests/fuzzers/fuzz_read.py
@@ -19,7 +19,8 @@ def TestOneInput(data):
 
 
 def main():
-    atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+    atheris.instrument_all()
+    atheris.Setup(sys.argv, TestOneInput)
     atheris.Fuzz()
 
 
