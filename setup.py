e104505d4b5a53b11fd0023c83829795d4b31853
Sebastian Wallkötter <sebastian@wallkoetter.net>
2022-01-28 16:44:30 +0100

    DEV: deprecate python 3.6 support (#740)
    * DEV: deprecate python 3.6 support
    
    * BUG: resolve py3.10 deprecation warnings
    
    * STY: black

--- setup.py
+++ setup.py
@@ -241,9 +241,9 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
