e785a5cd27973b23e09e63b4c54bbdbc990b3268
Sebastian Wallkötter <sebastian@wallkoetter.net>
2022-02-04 19:02:07 +0100

    MAINT: test py39 again but with path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ import imageio.plugins.swf
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
