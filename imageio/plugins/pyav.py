2083b6360d437160d0224db51d3a1f294b1a1f7a
Sebastian Wallkötter <sebastian@wallkoetter.net>
2022-03-08 15:18:40 +0100

    DOC: add missing word
    Co-authored-by: Almar Klein <almar@almarklein.org>

--- imageio/plugins/pyav.py
+++ imageio/plugins/pyav.py
@@ -233,7 +233,7 @@ class PyAVPlugin(PluginV3):
         it fits into memory) and to then access the returned ndarray randomly.
 
         The current implementation may cause problems for b-frames, i.e.,
-        bidirectionaly predicted pictures. I don't have any test videos this
+        bidirectionaly predicted pictures. I don't have any test videos of this
         though.
 
         ``format``s that do not have their pixel components byte-aligned, will
