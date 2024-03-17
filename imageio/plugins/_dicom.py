db8383ef21a988c61128585e49b6ae8852afdc3b
Pandede <steven9609@gmail.com>
2024-03-17 08:55:29 +0000

    fix series has no `ImagePositionPatient`

--- imageio/plugins/_dicom.py
+++ imageio/plugins/_dicom.py
@@ -703,7 +703,10 @@ class DicomSeries(object):
         self._entries.append(dcm)
 
     def _sort(self):
-        self._entries.sort(key=lambda k: (k.InstanceNumber, k.ImagePositionPatient[2]))
+        self._entries.sort(key=lambda k: (
+            k.InstanceNumber, 
+            getattr(k, 'ImagePositionPatient[2]', None)
+        ))
 
     def _finish(self):
         """
