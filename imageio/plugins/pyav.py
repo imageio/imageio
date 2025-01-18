c717105f8d433366a3f4a04bf897b3af4fa0227a
Sebastian Wallkötter <sebastian@wallkoetter.net>
2025-01-18 07:42:31 +0100

    other changes for av v14 compatibility

--- imageio/plugins/pyav.py
+++ imageio/plugins/pyav.py
@@ -258,6 +258,34 @@ def _get_frame_shape(frame: av.VideoFrame) -> Tuple[int, ...]:
     return tuple(shape)
 
 
+def _get_frame_type(picture_type: int) -> str:
+    """Return a human-readable name for provided picture type
+
+    Parameters
+    ----------
+    picture_type : int
+        The picture type extracted from Frame.pict_type
+
+    Returns
+    -------
+    picture_name : str
+        A human readable name of the picture type
+
+    """
+    picture_types = [
+        "None",
+        "I",
+        "P",
+        "B",
+        "S",
+        "SI",
+        "SP",
+        "BI",
+    ]
+
+    return picture_types[picture_type]
+
+
 class PyAVPlugin(PluginV3):
     """Support for pyAV as backend.
 
@@ -458,13 +486,19 @@ class PyAVPlugin(PluginV3):
 
             # reset stream container, because threading model can't change after
             # first access
-            self._video_stream.close()
             self._video_stream = self._container.streams.video[0]
 
             return frames
 
-        if thread_type is not None and thread_type != self._video_stream.thread_type:
+        if (
+            thread_type is not None
+            and not (
+                self._video_stream.thread_type == thread_type
+                or self._video_stream.thread_type.name == thread_type  
+            )
+        ):
             self._video_stream.thread_type = thread_type
+
         if (
             thread_count != 0
             and thread_count != self._video_stream.codec_context.thread_count
@@ -474,7 +508,10 @@ class PyAVPlugin(PluginV3):
             self._video_stream.codec_context.thread_count = thread_count
 
         if constant_framerate is None:
-            constant_framerate = not self._container.format.variable_fps
+            # "variable_fps" is now a flag (handle got removed). Full list at
+            # https://pyav.org/docs/stable/api/container.html#module-av.format
+            variable_fps = bool(self._container.format.flags & 0x400)
+            constant_framerate = not variable_fps
 
         # note: cheap for contigous incremental reads
         self._seek(index, constant_framerate=constant_framerate)
@@ -750,7 +787,10 @@ class PyAVPlugin(PluginV3):
             return metadata
 
         if constant_framerate is None:
-            constant_framerate = not self._container.format.variable_fps
+            # "variable_fps" is now a flag (handle got removed). Full list at
+            # https://pyav.org/docs/stable/api/container.html#module-av.format
+            variable_fps = bool(self._container.format.flags & 0x400)
+            constant_framerate = not variable_fps
 
         self._seek(index, constant_framerate=constant_framerate)
         desired_frame = next(self._decoder)
@@ -762,7 +802,7 @@ class PyAVPlugin(PluginV3):
                 "key_frame": bool(desired_frame.key_frame),
                 "time": desired_frame.time,
                 "interlaced_frame": bool(desired_frame.interlaced_frame),
-                "frame_type": desired_frame.pict_type.name,
+                "frame_type": _get_frame_type(desired_frame.pict_type),
             }
         )
 
@@ -781,10 +821,7 @@ class PyAVPlugin(PluginV3):
             self._flush_writer()
 
         if self._video_stream is not None:
-            try:
-                self._video_stream.close()
-            except ValueError:
-                pass  # stream already closed
+            self._video_stream = None
 
         if self._container is not None:
             self._container.close()
@@ -815,7 +852,7 @@ class PyAVPlugin(PluginV3):
         Parameters
         ----------
         codec : str
-            The codec to use, e.g. ``"libx264"`` or ``"vp9"``.
+            The codec to use, e.g. ``"h264"`` or ``"vp9"``.
         fps : float
             The desired framerate of the video stream (frames per second).
         pixel_format : str
@@ -850,7 +887,16 @@ class PyAVPlugin(PluginV3):
         if max_keyframe_interval is not None:
             stream.gop_size = max_keyframe_interval
         if force_keyframes is not None:
-            stream.closed_gop = force_keyframes
+            # this needs explanation: 
+            # The code sets the cgop (CLOSED_GOP) flag for the codec.
+            # Unfortunately, the current version of pyAV interprets the flags
+            # (and flags2) fields as type "long" not "ulong", as such the
+            # otherwise neat flag value of 0x80000000 converts to the (signed)
+            # value of -2147483648
+            if force_keyframes:
+                stream.codec_context.flags |= -2147483648
+            else:
+                stream.codec_context.flags &= ~-2147483648
 
         self._video_stream = stream
 
