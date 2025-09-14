cba765b00e58caf1cf2d59ae4144954d377ecefc
Sebastian Wallkotter <sebastian@wallkoetter.net>
2025-09-14 08:45:12 +0200

    skip dash tests (for now)

--- tests/test_pyav.py
+++ tests/test_pyav.py
@@ -438,8 +438,8 @@ def test_sequential_reading(test_images):
 
     assert np.allclose(actual_imgs, expected_imgs)
 
-
-def test_uri_reading(test_images):
+@pytest.mark.skip(reason="PyAV no longer supports DASH natively. This needs separate investigation.")
+def test_uri_reading(test_images):    
     uri = "https://dash.akamaized.net/dash264/TestCases/2c/qualcomm/1/MultiResMPEG2.mpd"
 
     with av.open(uri) as container:
